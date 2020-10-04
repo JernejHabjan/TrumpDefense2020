31994eda41a74eaa7903a759ce4a1cb3cb295017
Jernej <jernej.habjan96@gmail.com>
2020-10-04 12:59:56 +0200

    Targets and uproject update

--- TD2020/Source/TD2020Server.Target.cs
+++ TD2020/Source/TD2020Server.Target.cs
@@ -8,20 +8,6 @@ public class TD2020ServerTarget : TargetRules
     public TD2020ServerTarget(TargetInfo target) : base(target)
     {
         Type = TargetType.Server;
-        //BuildEnvironment = TargetBuildEnvironment.Shared;
-        //bOutputToEngineBinaries = true;
         ExtraModuleNames.AddRange(new string[] {"TD2020"});
     }
-
-    //
-    // TargetRules interface.
-    //
-
-    //public override void SetupGlobalEnvironment(
-    //    TargetInfo Target,
-    //    ref LinkEnvironmentConfiguration OutLinkEnvironmentConfiguration,
-    //    ref CPPEnvironmentConfiguration OutCPPEnvironmentConfiguration
-    //    )
-    //{
-    //}
 }
